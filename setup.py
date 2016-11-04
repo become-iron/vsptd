97e814952c66ee3cfd01f358785eae2912b693b3
become-iron <nikitaburkov@yandex.ru>
2016-11-05 00:27:18 +0300

    релиз версии 1.2.0

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 setup(
     name='vsptd',
     description='Работа с ВСПТД',
-    version='1.1.0',
+    version='1.2.0',
     url='https://github.com/become-iron/vsptd/',
-    long_description=open('README.rst', encoding='utf-8').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     classifiers=['Natural Language :: Russian'],
     py_modules=['vsptd'],
     data_files=[('unittests', ['unittests/ut_vsptd.py']),
-                ('help', ['README.rst'])
+                ('help', ['README.md'])
                 ],
 )
