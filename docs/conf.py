c0b09f508b15396822325bc521b82e0f34e4f149
become-iron <nikitaburkov@yandex.ru>
2017-01-29 15:48:36 +0300

    обновление документации

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@
 # -- General configuration ------------------------------------------------
 
 import sys
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 
 sys.path.append('../')
 # sys.path.append('../vsptd/')
@@ -38,9 +38,11 @@ sys.path.append('../')
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
-    'sphinx.ext.githubpages']
+    # 'sphinx.ext.githubpages'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -92,9 +94,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'default'
+# html_theme = "sphinx_rtd_theme"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
